e3d8629359922dc5ad4ade6305f4eb27ee8ce1af
Brightmoon987 mezer <azzaretto.jan@iisgalvanimi.edu.it>
2024-12-02 11:57:54 +0000

    2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+global lista_spesa
 
 from flask import Flask, render_template
 
@@ -10,6 +10,68 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+
+
+
+# route per aggiungere elementi alla lista
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+elemento = request.form['elemento']
+if elemento:
+lista_spesa.append(elemento)
+return redirect(url_for('home'))
+
+
+#route per rimuovere elementi grazie all'indice
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+if 0 <= indice < len(lista_spesa):
+lista_spesa.pop(indice)
+return redirect(url_for('home'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #avvio dell'app Flask
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
